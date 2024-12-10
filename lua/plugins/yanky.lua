8d2109e18c2d00556e3e8d89eee08c4280afb516
Akash Kennedy <akashkennedy1@gmail.com>
2024-12-10 14:54:51 +0530

    Add waktime and yanky Plugins

---
+++ lua/plugins/yanky.lua
@@ -0,0 +1,12 @@
+return {
+	"gbprod/yanky.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		highlight = {
+			on_put = true,
+			on_yank = true,
+			timer = 100,
+		},
+	},
+}
