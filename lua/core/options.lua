32f79f2b14c8c8451118d676a02ca5882f007769
Akash Kennedy <akashkennedy1@gmail.com>
2025-01-10 15:58:16 +0530

    Update Config

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -20,6 +20,9 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
 -- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
